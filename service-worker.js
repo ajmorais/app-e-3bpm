2d719d90fb0f98ef323825339c3bda6cbbf8ffbb
ajmorais <110871691+ajmorais@users.noreply.github.com>
2026-03-29 15:42:15 -0300

    Refactor service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', function(event) {
-  // apenas passa a requisição (necessário para PWA)
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('fetch', () => {});
